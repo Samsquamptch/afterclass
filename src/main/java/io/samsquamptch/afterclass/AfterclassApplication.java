b2549c8e42f51a7255609f9617be72b5680abbfc
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-05-19 12:39:20 +0100

    spring boot
    added spring initialiser files

---
+++ src/main/java/io/samsquamptch/afterclass/AfterclassApplication.java
@@ -0,0 +1,13 @@
+package io.samsquamptch.afterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/samsquamptch/afterclass/AfterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package io.samsquamptch.afterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
