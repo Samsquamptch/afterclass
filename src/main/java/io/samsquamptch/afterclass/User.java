6e5c98e6c6613641fa38ed9add45549781df1d8e
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-05-20 13:50:19 +0100

    set attributes and methods for user

--- src/main/java/io/samsquamptch/afterclass/User.java
+++ src/main/java/io/samsquamptch/afterclass/User.java
@@ -1,4 +1,25 @@
 package io.samsquamptch.afterclass;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class User {
+    @Setter
+    private String name;
+    @Setter
+    private int id;
+    private List<Lesson> lessons = new ArrayList<Lesson>();
+
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void addLesson(Lesson lesson) {this.lessons.add(lesson);}
+
+    public void removeLesson(Lesson lesson) {this.lessons.remove(lesson);}
 }
