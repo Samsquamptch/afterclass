fa9f2984970318c060f91f1da490aa7d4bbf2e98
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-05-22 13:50:51 +0100

    added findby methods for GrouRepository

--- src/main/java/io/samsquamptch/afterclass/repositories/GroupRepository.java
+++ src/main/java/io/samsquamptch/afterclass/repositories/GroupRepository.java
@@ -2,6 +2,12 @@ package io.samsquamptch.afterclass.repositories;
 
 import io.samsquamptch.afterclass.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends JpaRepository <Group, Long> {
+
+    Group findByPassCode(String passCode);
+
+    Group findById(long id);
 }
--- src/main/java/io/samsquamptch/afterclass/repositories/UserRepository.java
+++ src/main/java/io/samsquamptch/afterclass/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package io.samsquamptch.afterclass.repositories;
 
 import io.samsquamptch.afterclass.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
