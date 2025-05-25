7a022dfd47fc4c113899e96e0dd66f457a42d844
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-05-25 23:15:11 +0100

    Created stubs for Group test cases. Updated Group entity to fix JPA relational issues.

--- src/test/java/io/samsquamptch/afterclass/service/GroupServiceTests.java
+++ src/test/java/io/samsquamptch/afterclass/service/GroupServiceTests.java
@@ -1,4 +1,27 @@
 package io.samsquamptch.afterclass.service;
 
-public class GroupServiceTests {
+import io.samsquamptch.afterclass.services.GroupService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class GroupServiceTests extends AbstractIntegrationTests {
+
+    @Autowired
+    private GroupService groupService;
+
+    @Test
+    void testCreateGroup() {}
+
+    @Test
+    void testGetGroup() {}
+
+    @Test
+    void testUpdateGroup() {}
+
+    @Test
+    void testDeleteGroup() {}
 }
