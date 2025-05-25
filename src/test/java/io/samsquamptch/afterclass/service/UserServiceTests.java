3d661fcb24389879d53a87562d08e4d4eb1a115f
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-05-25 17:53:55 +0100

    Added stubs for User Service test cases

--- src/test/java/io/samsquamptch/afterclass/service/UserServiceTests.java
+++ src/test/java/io/samsquamptch/afterclass/service/UserServiceTests.java
@@ -1,4 +1,30 @@
 package io.samsquamptch.afterclass.service;
 
-public class UserServiceTests {
+import io.samsquamptch.afterclass.services.UserService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class UserServiceTests extends AbstractIntegrationTests {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void getUser() {}
+
+    @Test
+    public void getAllUsers() {}
+
+    @Test
+    public void addUser() {}
+
+    @Test
+    public void updateUser() {}
+
+    @Test
+    public void deleteUser() {}
 }
