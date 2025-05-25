35a7edffd192c1114494b33e65a671ca0ee0f414
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-05-25 23:22:35 +0100

    Added getGroup and addGroup test cases. Both pass.

--- src/test/java/io/samsquamptch/afterclass/service/GroupServiceTests.java
+++ src/test/java/io/samsquamptch/afterclass/service/GroupServiceTests.java
@@ -1,10 +1,13 @@
 package io.samsquamptch.afterclass.service;
 
+import io.samsquamptch.afterclass.dto.GroupDTO;
 import io.samsquamptch.afterclass.services.GroupService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -14,14 +17,26 @@ public class GroupServiceTests extends AbstractIntegrationTests {
     private GroupService groupService;
 
     @Test
-    void testCreateGroup() {}
+    void getGroup() {
+        GroupDTO groupDTO = groupService.getGroup(1L);
+        assertEquals("Test Group", groupDTO.getName());
+        assertEquals("zQxprL", groupDTO.getPassCode());
+        assertEquals(2, groupDTO.getUsers().size());
+    }
 
     @Test
-    void testGetGroup() {}
+    void addGroup() {
+        GroupDTO groupDTO = groupService.createGroup("Birkbeck Socials");
+        assertEquals(2L, groupDTO.getId());
+        assertEquals("Birkbeck Socials", groupDTO.getName());
+        assertEquals(6, groupDTO.getPassCode().length());
+        assertEquals(0, groupDTO.getUsers().size());
+    }
+
 
     @Test
-    void testUpdateGroup() {}
+    void updateGroup() {}
 
     @Test
-    void testDeleteGroup() {}
+    void deleteGroup() {}
 }
