4521eb34374fcbda33d25cb27d6c73d8ce0953ff
Samscranch <140660422+Samscranch@users.noreply.github.com>
2025-06-05 22:01:30 +0100

    Added React and Router dependencies, along with basic outline of pages

---
+++ web/src/App.js
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Groups from "./pages/Groups";
+import About from "./pages/About";
+import NoPage from "./pages/NoPage";
+
+function App() {
+
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="groups" element={<Groups />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
